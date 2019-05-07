8e912d325c4b0a6f272c7c3d37b7ed40d0ba1f58
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-07 10:12:13 +0200

    wip

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('💥 hello world!!! 💥');
+
+// this is declaration
+var foo;
+// this is assignment
+foo = 5;
+// declaration plus assignment
+var bar = 7;
+// using let. can't re-declare
+let myVarWithLet = 7;
+// using let. can re-assign
+myVarWithLet = 5;
+// using const. can't re-declare
+const myVarWithConst = true;
+// using const. can't re-assing either
+myVarWithConst = false;
